7652a99ce59bb86ac03adf7c7121c934b37ae1cd
rafaelrgb <faerugb@gmail.com>
2017-03-30 00:54:37 -0300

    Pwm motion of the wheels

--- max_motor_driver/src/motor_driver.cpp
+++ max_motor_driver/src/motor_driver.cpp
@@ -1,92 +1,100 @@
 #include <wiringPi.h>
 #include "ros/ros.h"
-#include "max_msgs/MovementCommand.h"
+#include "geometry_msgs/Twist.h"
 
-// Function to move the robot forward
-void forward()
-{
-    digitalWrite(0, LOW);
-    digitalWrite(1, HIGH);
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-}
+#define IN1 0
+#define IN2 1
+#define IN3 2
+#define IN4 3
+#define ENA 4
+#define ENB 5
 
-// Function to move the robot backward
-void backward()
-{
-    digitalWrite(0, HIGH);
-    digitalWrite(1, LOW);
-    digitalWrite(2, LOW);
-    digitalWrite(3, HIGH);
-}
+#define MAX_LINEAR 5.0
+#define MAX_ANGULAR 5.0
 
-// Function to stop the robot
-void stop() 
-{
-    digitalWrite(0, LOW);
-    digitalWrite(1, LOW);
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-}
+#define R 0.065
+#define L 0.132
 
-// Function to rotate the robot to the right
-void right()
+// Function to drive the left wheel
+void driveLeftWheel( double speed )
 {
-    digitalWrite(0, LOW);
-    digitalWrite(1, HIGH);
-    digitalWrite(2, LOW);
-    digitalWrite(3, HIGH);
-}
-
-// Function to rotate the robot to the left
-void left()
-{
-    digitalWrite(0, HIGH);
-    digitalWrite(1, LOW);
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-}
-
-// Callback function to the Movement Command subscriber
-void movementCommandCb(const max_msgs::MovementCommand::ConstPtr& msg)
-{
-  switch( msg->command )
+  if ( speed > 0 )
   {
-	  case max_msgs::MovementCommand::FORWARD:
-		forward();
-		break;
-	  case max_msgs::MovementCommand::BACKWARD:
-		backward();
-		break;
-	  case max_msgs::MovementCommand::STOP:
-		stop();
-		break;
-	  case max_msgs::MovementCommand::LEFT:
-		left();
-		break;
-	  case max_msgs::MovementCommand::RIGHT:
-		right();
-		break;
-	  default:
-		stop();
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
   }
+  else if ( speed > 0 )
+  {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  }
+  else if ( speed == 0 )
+  {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+  }
+  softPwmWrite(ENA, speed);
+}
+
+// Function to drive the right wheel
+void driveRightWheel( double speed )
+{
+  // Drive the left wheel forward with increading velocity
+  if ( speed > 0 )
+  {
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  }
+  else if ( speed > 0 )
+  {
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
+  else if ( speed == 0 )
+  {
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  }
+  softPwmWrite(ENB, speed);
+}
+
+// Callback function to the command velocity
+void cmdVelCb(const geometry_msgs::Twist::ConstPtr& msg)
+{
+  double phi1, phi2;
+  phi1 = (1.0 / R) * msg->linear.x + (L / R) * msg->angular.z;
+  phi2 = (1.0 / R) * msg->linear.x + (-1.0 * L / R) * msg->angular.z;
+
+
+  phi1 = phi1 * 100.0 / 87.077;
+  phi2 = phi2 * 100.0 / 87.077;
+
+
+  driveLeftWheel( phi1 );
+  driveRightWheel( phi2 );
 }
 
 int main(int argc, char** argv)
 {
   // Configure GPIO pins
   wiringPiSetup();
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  softPwmCreate(ENA, 0, 100);
+  softPwmCreate(ENB, 0, 100);
   
   // Init the ROS node
   ros::init(argc, argv, "motor_driver");
   ros::NodeHandle n;
   
   // Subscribe to the movement command topic
-  ros::Subscriber movement_command_sub = n.subscribe("movement_command", 1000, movementCommandCb);
+  ros::Subscriber cmd_vel_sub = n.subscribe("cmd_vel", 1000, cmdVelCb);
   
   ros::spin();
   
--- max_teleop/src/max_teleop_node.cpp
+++ max_teleop/src/max_teleop_node.cpp
@@ -1,6 +1,9 @@
 #include "joystick.hh"
 #include "ros/ros.h"
-#include "max_msgs/MovementCommand.h"
+#include "geometry_msgs/Twist.h"
+
+#define MAX_LINEAR 5.0
+#define MAX_ANGULAR 5.0
 
 int main(int argc, char **argv)
 {
@@ -10,56 +13,47 @@ int main(int argc, char **argv)
 	// Ensure that it was found and that we can use it
 	if (!joystick.isFound())
 	{
-		printf("open failed.\n");
+    printf("Joystick open failed.\n");
 		exit(1);
 	}
   
-	// Init te ros node
+  // Init the ros node
 	ros::init(argc, argv, "max_teleop_node");
 	ros::NodeHandle n;
 	
 	// Create a publisher to send the movement command msgs
-	ros::Publisher movement_command_pub = n.advertise<max_msgs::MovementCommand>("movement_command", 1000);
+  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
 
-	ros::Rate loop_rate(10);
+  ros::Rate loop_rate(1000);
 
+  double x = 0.0, z = 0.0;
 
-  // Variable to store the last command
-  int command = max_msgs::MovementCommand::STOP;
-
-	while (ros::ok())
+  while (ros::ok())
 	{
 		// Create the movement command msg
-    max_msgs::MovementCommand msg;
+    geometry_msgs::Twist msg;
 		
 		// Attempt to sample an event from the joystick
 		JoystickEvent event;
 		if (joystick.sample(&event))
 		{
-		  if (event.isButton())
+      if (event.isAxis())
 		  {
-        if (event.value == 0)
+        if (event.number == 3)
         {
-          command = max_msgs::MovementCommand::STOP;
-        } else if (event.number == 0)
-        {
-          command = max_msgs::MovementCommand::FORWARD;
-        } else if (event.number == 1)
-        {
-          command = max_msgs::MovementCommand::RIGHT;
-        } else if (event.number == 2)
-        {
-          command = max_msgs::MovementCommand::BACKWARD;
-        } else if (event.number == 3)
-        {
-          command = max_msgs::MovementCommand::LEFT;
+          x = -1.0 * static_cast<double>(event.value) * MAX_LINEAR / 32767.0;
         }
+        if (event.number == 2)
+        {
+          z = static_cast<double>(event.value) * MAX_ANGULAR / 32767.0;
+        }
+
+        // Publish the message
+        msg.linear.x = x;
+        msg.angular.z = z;
+        cmd_vel_pub.publish(msg);
 		  }
 		}
-		
-		// Publish the message
-    msg.command = command;
-		movement_command_pub.publish(msg);
 
 		ros::spinOnce();
 
